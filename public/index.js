4c3cc05b508c5d0282509ec52d3b96695dbc71e4
cfdebug <rfadventure22@gmail.com>
2022-08-23 19:31:29 -0500

    added local server storage

--- public/index.js
+++ public/index.js
@@ -1,22 +1,25 @@
-function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+async function main() {
+  const countContainer = document.querySelector("#count-container");
+  const incrementButton = document.querySelector("#increment-button");
+  const decrementButton = document.querySelector("#decrement-button");
 
-    let countValue = 0;
+  let currCount = await fetch("http://localhost:9001/counter");
+  let countObj = await currCount.json();
+  console.log(countObj);
+  let countValue = countObj.value;
 
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
-    }
-
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
-    }
-
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+  function increment() {
+    countValue++;
     countContainer.textContent = countValue;
+  }
+
+  function decrement() {
+    countValue--;
+    countContainer.textContent = countValue;
+  }
+
+  incrementButton.addEventListener("click", increment);
+  decrementButton.addEventListener("click", decrement);
+  countContainer.textContent = countValue;
 }
-main()
+main();
